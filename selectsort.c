f4ccadcb55f7c472119e8860883f2e9be73abd1e
dhruvag02 <53806394+dhruvag02@users.noreply.github.com>
2020-02-13 13:00:46 +0530

    Add files via upload

---
+++ selectsort.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#define S 100
+#include<time.h>
+void selectsort(int arr[S],int n);
+void swap(int *xp,int *yp);
+int main()
+{
+int arr[S],i,j,n,k;
+clock_t time;
+printf("Enter number of elements to be inserted in array\n");
+scanf("%d",&n);
+printf("Enter the elements\n");
+for(i=0;i<n;i++)
+{
+scanf("%d",&arr[i]);
+}
+time=clock();
+selectsort(arr,n);
+time=clock()-time;
+printf("Time required to complete bubble sort:%f seconds\n",(float)time/CLOCKS_PER_SEC);
+printf("Elements in sorted order\n");
+for(i=0;i<n;i++)
+{
+printf("%d\n",arr[i]);
+}
+printf("Enter number 'k' to view k largest elements\n");
+scanf("%d",&k);
+printf("*******\n");
+for(j=n-1;k>0;j--)
+{
+printf("%d\n",arr[j]);
+k--;
+}
+return 0;
+}
+
+void selectsort(int arr[S],int n)
+{
+int i,j,temp,min_ind,small;
+for(i=0;i<n-1;i++)
+{
+min_ind=i;
+small=arr[i];
+for(j=i+1;j<n;j++)
+{
+if(arr[j]<small)
+{
+small=arr[j];
+min_ind=j;
+}
+}
+swap(&arr[min_ind],&arr[i]);
+}
+}
+
+void swap(int *xp,int *yp)
+{
+int temp;
+temp=*xp;
+*xp=*yp;
+*yp=temp;
+}
+
+
+
+

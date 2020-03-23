1d2c0d1ba7dd125f7c4d058403b8d2ad2d8a43b4
dhruvag02 <53806394+dhruvag02@users.noreply.github.com>
2020-03-23 10:33:30 +0530

    Add files via upload

---
+++ FLOYD'S.C
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<conio.h>
+#define S 20
+int min(int a,int b);
+int main()
+{
+int n,i,j,k;
+int adj[S][S],d[S][S];
+printf("Enter number of vertices\n");
+scanf("%d",&n);
+printf("Enter the adjacency matrix\n");
+for(i=1;i<=n;i++)
+{
+	for(j=1;j<=n;j++)
+		scanf("%d",&adj[i][j]);
+	printf("\n");
+}
+for(i=1;i<=n;i++)
+	for(j=1;j<=n;j++)
+		d[i][j]=adj[i][j];
+
+for(k=1;k<=n;k++)
+{
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+			d[i][j]=min(d[i][j],d[i][k]+d[k][j]);
+		}
+	}
+}
+printf("Distance matrix is displayed\n");
+for(i=1;i<=n;i++)
+{
+	for(j=1;j<=n;j++)
+		printf("%d\t",d[i][j]);
+	printf("\n");
+}
+getch();
+return 0;
+}
+
+int min(int a,int b)
+{
+if(a<b)
+return a;
+else
+return b;
+}
